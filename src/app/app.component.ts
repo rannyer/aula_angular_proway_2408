bde42a6f2c06715fb3f1180f17f40b75c036d053
unknown <RANNYER.THOM@proway.treina>
2024-08-24 12:31:51 -0300

    Alteracoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/exemplo-card/card/card.component';
+import { CommonModule } from '@angular/common';
+import { ListCardComponent } from "./components/exemplo-card/list-card/list-card.component";
+import { ListMusicaComponent } from "./components/exercicio-musica/list-musica/list-musica.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, CommonModule, ListCardComponent, ListMusicaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projeto-01';
+  title = 'abc';
+ 
+  nomes = ["Pedro","Sylvana", "Jaina","Anduim","José","Maria"]
+  filmes = ["Filme 1","Filme 2", "Filme 3","Filme 4"]
+  frutas = ["Mamão","Maçã","Manga","Maracujá"]
+
+  url = "https://pbs.twimg.com/media/CsUzcXqWYAARYZm.jpg"
+
+  rodarAlerta(){
+    if(this.url == "https://pbs.twimg.com/media/CsUzcXqWYAARYZm.jpg"){
+      this.url = "https://pbs.twimg.com/media/EjNC9LJXsAA87Dh?format=jpg&name=large"
+    }else{
+      this.url = "https://pbs.twimg.com/media/CsUzcXqWYAARYZm.jpg"
+    }
+  }
 }
